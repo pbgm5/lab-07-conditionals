6e72723100c64a760957bc6f579f365ea67a3251
pbgm5 <Priya.Begum@mail.citytech.edu>
2021-03-22 16:18:56 -0400

    stuff

--- cake.py
+++ cake.py
@@ -1,8 +1,8 @@
-cakeorbust = input"Do you want cake? yes or no"
+cakeorbust = input("Do you want cake? yes or no")
 
 if cakeorbust == "yes" or cakeorbust == "YES" or cakeorbust == "y":
-     print"Have some cake"
+     print("Have some cake")
 elif cakeorbust == "no" or cakeorbust == "NO" or cakeorbust == "n":
-     print"no cake for you!"
+     print("no cake for you!")
  else:
-    print"Sorry, I don't understand."
+    print("Sorry, I don't understand.")
