e97b779e9d053ba2bd3ed85d4c70264b84315426
pbgm5 <Priya.Begum@mail.citytech.edu>
2021-04-03 16:18:19 -0400

    stuff added

---
+++ oddcalc.py
@@ -0,0 +1,10 @@
+operation = input("And what operation do you want? (mul/div/mod");
+
+if (operation === "mul"):
+    print(firstnum * secondnum):
+elif (operation === "div")
+    print(firstnum / secondnum):
+elif (operation === "mod")
+    print(firstnum % secondnum):
+   else:
+       print();
--- reminders.v02.py
+++ reminders.v02.py
@@ -1,12 +1,16 @@
-usertime = int(input("what hour is it? (0-23)"))
+imort datetime
 
-if usertime <= 5:
+now.hour = int(input("what hour is it? (0-23)"))
+
+now = datetime.now()
+print(now.year, now.motnh, now.day, now.hour, now.minute, now.second)
+if now.hour <= 5:
     print("Its early. You should be sleeping!")
-elif usertime <= 7:
+elif now.hour <= 7:
     print("Wake up, brew that coffe, get that mile run in, and get that breakfast...")
-elif usertime <=9:
+elif now.hour <= 9:
     print("Time to go to work.")
-elif usertime <=12:
+elif now.hour <= 12:
     print()
 else:
         print("You didn't type an acceptable value! (0-23)")
